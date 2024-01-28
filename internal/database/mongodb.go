81e52d1760bd99db636aac1398af5b56c300de14
ymw0407 <yunminwo1211@gmail.com>
2024-01-28 17:09:09 +0900

    Init : MongoDB Setting added
    Signed-off-by: ymw0407 <yunminwo1211@gmail.com>

---
+++ internal/database/mongodb.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	mongodbURI      string
+	mongodbDatabase string
+
+	Client    *mongo.Client
+	ClientErr error
+
+	UsersColl = &mongo.Collection{}
+)
+
+func Initialize() {
+	mongodbURI = os.Getenv("MONGODB_URI")
+	mongodbDatabase = os.Getenv("MONGODB_DATABASE")
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(mongodbURI).SetServerAPIOptions(serverAPI)
+
+	Client, ClientErr = mongo.Connect(context.TODO(), opts)
+	if ClientErr != nil { // client Connection에서 에러가 발생하면
+		panic(ClientErr)
+	}
+	log.Println("MongoDB connected successfully!")
+
+	defineCollection()
+}
+
+func defineCollection() {
+	UsersColl = Client.Database(mongodbDatabase).Collection("users")
+}
